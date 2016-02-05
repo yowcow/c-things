20cf7059d630e0796352a2e396c6116652a4cc70
yowcow <yowcow@cpan.org>
2016-02-05 04:47:49 +0000

    struct

--- src/hello-test.c
+++ src/hello-test.c
@@ -4,8 +4,7 @@
 
 #include "hello.h"
 
-int main() {
-
+int test_get_message() {
     assert(
         strcmp(
             get_message("Hoge"),
@@ -13,7 +12,27 @@ int main() {
         ) == 0
     );
 
-    puts("-- done-testing");
+    return 1;
+}
+
+int test_create_member() {
+    struct member m;
+    m = create_member(1234, "hoge");
+
+    assert(m.id == 1234);
+    assert(strcmp(m.name, "hoge") == 0);
+
+    return 2;
+}
+
+int main() {
+    int count;
+    count = 0;
+
+    count += test_get_message();
+    count += test_create_member();
+
+    printf("-- done-testing: %d things\n", count);
 
     return 0;
 }
--- src/hello.h
+++ src/hello.h
@@ -2,13 +2,22 @@
 #define HELLO_H_
 
 #include <stdlib.h>
+#include <stdio.h>
 
-char* get_message(char*);
+struct member {
+    int id;
+    char* name;
+};
 
 char* get_message(char* name) {
     char* message = (char *) malloc(sizeof(char) * 100);
     sprintf(message, "Hello world, %s", name);
     return message;
+}
+
+struct member create_member(int id, char* name) {
+    struct member mem = { id, name };
+    return mem;
 }
 
 #endif
